26b731f6d4a6c892b4cff21ca40491eae2f72914
WIN-QUG5H71A7UD <109420678+OlexaGryschenko@users.noreply.github.com>
2026-08-05 23:40:38 +0100

    chore: add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'modern-normalize/modern-normalize.css';
 import './index.css'
 import App from './components/App/App.tsx'
+
 
 const queryClient = new QueryClient();
 
